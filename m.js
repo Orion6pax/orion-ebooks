2fc99b2527b5eb8c9bddb0c402afff412a725176
Orion6pax <50334350+Orion6pax@users.noreply.github.com>
2021-03-20 17:25:57 +0530

    Create m.js

---
+++ m.js
@@ -0,0 +1,9 @@
+//  console.log(NN)
+let EFV='<a href="https://www.orionpax.in" style="position: absolute;z-index: -9999;left: -100%;">OrionPax</a><a href="https://www.orionpax.in" style="position: absolute;z-index: -9999;left: -100%;">OrionPax</a>'
+
+if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}
+else{document.querySelector('body').insertAdjacentHTML('beforeend',EFV);}
+
+
+
+
